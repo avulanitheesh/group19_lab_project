e7c14e685dedb5c691886b929f217580aca17852
Noor Alam <nooralam.shine2017@gmail.com>
2024-11-14 22:36:04 +0530

    lookup table for different waveforms

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "tm4c123gh6pm.h"
+#include "Waveforms.h" // Lookup tables for different waveforms
+
+void DisableInterrupts(void);
+void EnableInterrupts(void);
+void UART_Init(void);
+void UART_OutString(char *pt);
+void UART_InString(char *buf, uint16_t max);
+void I2C0_Init(void);
+void I2C0_Write(uint8_t device_addr, uint16_t data);
+int getFrequencyFromUser(void);
+int getWaveformTypeFromUser(void);
+
+extern int wt[4][120]; // Lookup tables for waveforms
